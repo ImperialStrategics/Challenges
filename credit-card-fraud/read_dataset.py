a30748ef5b924370796bb2f71792567229c98e00
Adam Bhaiji <adam.bhaiji16@imperial.ac.uk>
2018-07-20 11:28:00 +0100

    Change names and directories

--- credit-card-fraud/read_dataset.py
+++ credit-card-fraud/read_dataset.py
@@ -2,6 +2,6 @@
 import pandas as pd
 
 # create a dataframe
-df = pd.read_csv('./credit-card-fraud/dataset.csv')
+df = pd.read_csv('./dataset.csv')
 # output first 5 rows in dataset
 print(df.head())
